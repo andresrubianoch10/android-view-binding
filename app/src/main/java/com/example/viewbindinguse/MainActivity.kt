e656a231fb5a86ff8ac0cd65b5a0d670da2a7037
andres.rubiano <andres.rubiano@globant.com>
2022-05-21 17:04:12 -0500

    Implementing ViewBinding in Activities

--- app/src/main/java/com/example/viewbindinguse/MainActivity.kt
+++ app/src/main/java/com/example/viewbindinguse/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.viewbindinguse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.viewbindinguse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.textViewInitViewLabel.text = "Changed text to test view binding lib"
     }
 }
